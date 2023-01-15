5a6ecd5f8f3ab6db70ebd1120ed1c87585aea9a0
okolipraiz <okolipraiz@gmail.com>
2023-01-15 08:47:47 +0100

    remove .env and added hello world

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,7 @@ export default function Home() {
       <Fragment>
           <main className="container">
               <div >
-                  <h1>Start Here</h1>
-                  <p>Lorem yieieie oeuwuupeiwuiwe ggg</p>
-                  <p>Is there need for password recovery?</p>
-                  <button className="btn btn-primary">Testing bootstrap</button>
+                  <h1>Hello World</h1>
               </div>
           </main>
       </Fragment>
