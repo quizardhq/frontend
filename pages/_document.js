bb50d7eb75b812ab87615ae9231c01ce12378395
Divine Orji <dpkreativ@gmail.com>
2023-03-04 12:56:58 +0100

    bug-fix: fixes crossorigin bug

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head />
-            <link
-                href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-                rel="stylesheet"
-                integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-                crossorigin="anonymous"
-            />
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html lang="en">
+      <Head />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+        crossOrigin="anonymous"
+      />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
