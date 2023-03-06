f1b35bb18be9aa4a1a7241a9151d19f0634aa438
Divine Orji <dpkreativ@gmail.com>
2023-03-06 12:32:50 +0100

    refactor: updates noise to match the design

--- components/temp/ui/organisms/Header.jsx
+++ components/temp/ui/organisms/Header.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function Header() {
-  return (
-    <div className="header bg-green-300 bg-[url(/assets/noise.svg)] filter-[contrast(70%)_brightness(1000%) opacity-5] text-red-300">
-      Head
-    </div>
-  );
+  return <div className="bg-green-300 bg-[url(/assets/noise.svg)]">Head</div>;
 }
