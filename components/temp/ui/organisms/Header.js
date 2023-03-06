2f03f22b6394742caaf2cf1131d65dfa95946e9e
Divine Orji <dpkreativ@gmail.com>
2023-03-06 11:18:20 +0100

    feat: tweaks noise filter in Header component

--- components/temp/ui/organisms/Header.js
+++ components/temp/ui/organisms/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Header() {
-  return <div className="header text-red-300">Head</div>;
+  return (
+    <div className="header bg-green-300 bg-[url(/assets/noise.svg)] filter-[contrast(70%)_brightness(1000%) opacity-5] text-red-300">
+      Head
+    </div>
+  );
 }
