2ae9476063294f48aaf1a8fbf9d854e8d4e73d92
Divine Orji <dpkreativ@gmail.com>
2023-03-06 12:01:14 +0100

    refactor: removes Bootstrap
    In this commit, I removed Bootstrap's code so we can work with Tailwind

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-        crossOrigin="anonymous"
-      />
+
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import { Fragment } from 'react';
 
-
 export default function Home() {
   return (
-      <Fragment>
-          <main className="container">
-              <div >
-                  <h1>Hello World</h1>
-              </div>
-          </main>
-      </Fragment>
+    <Fragment>
+      <main>
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      </main>
+    </Fragment>
   );
 }
