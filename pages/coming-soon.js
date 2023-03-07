8729c063b33d8e504cb9d8c68e877b8c736d874e
Divine Orji <dpkreativ@gmail.com>
2023-03-07 16:43:13 +0100

    feat: implements hero section on coming-soon page
    In this commit, I did the following:
    - Built the hero section of the coming soon page
    - Added svg images for the hero illustration and background

--- pages/coming-soon.js
+++ pages/coming-soon.js
@@ -3,7 +3,53 @@ import HomeLayout from '@/components/temp/layouts/HomeLayout';
 export default function ComingSoon() {
   return (
     <HomeLayout>
-      <div>Coming soon</div>
+      {/* --- Hero Section --- */}
+      <section className="px-32 py-20 bg-[url(/assets/waves.svg)] bg-no-repeat bg-bottom min-h-screen">
+        <div className="max-w-7xl mx-auto grid grid-cols-2 items-center">
+          {/* Left side */}
+          <div className="grid gap-5">
+            <h1 className="font-semibold text-5xl leading-snug">
+              Engage & Interact with your Audience
+            </h1>
+            <p className="text-2xl font-light">
+              Quizard is an all-in-one platform for engaging your audience with
+              interactive quiz sessions and collect valuable results/feedback.
+            </p>
+
+            {/* Waitlist form */}
+            <div>
+              <form>
+                <label htmlFor="" className="text-sm">
+                  Join the waitlist to get notified when we launch
+                </label>
+                <div className="w-full max-w-md flex border border-[#00A65B] rounded-md p-1">
+                  <input
+                    type="email"
+                    name="waitlist_email"
+                    id="waitlist_email"
+                    placeholder="Email address"
+                    className="w-full outline-none bg-transparent rounded-md m-3"
+                  />
+                  <button
+                    type="submit"
+                    className="bg-[#00ED85] text-green-200 px-8 py-3 rounded-md"
+                  >
+                    Join
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          {/* Right side */}
+          <div>
+            <img
+              src="/assets/illustrations/coming-soon_hero.svg"
+              alt="An illustration of four human characters interacting with each other"
+            />
+          </div>
+        </div>
+      </section>
     </HomeLayout>
   );
 }
