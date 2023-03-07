cb2107c365d1f4064e8e1b8ae20934830ddf025b
Divine Orji <dpkreativ@gmail.com>
2023-03-07 15:37:27 +0100

    refactor: tweaks `<Header>` and uses semantic html
    Here I did the following:
    - Added semantic tags to the header
    - Set the max width so it doesn't look out of place on larger screens
    - Update the background color to match the UI
    - Set the header to stick to the top on scroll

--- components/temp/ui/organisms/Header.jsx
+++ components/temp/ui/organisms/Header.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="bg-green-300 bg-[url(/assets/noise.svg)] flex justify-between px-32 py-5 items-center text-[#00391B] font-medium">
-      {/* Logo */}
-      <div>
-        <img src="/assets/logo-full.svg" alt="Quizard logo" />
-      </div>
-
-      {/* Nav */}
-      <div className="flex space-x-5 items-center">
-        <div>Features</div>
-        <div>About us</div>
-        <div>FAQ</div>
+    <header className="bg-[#C4FBDD] bg-[url(/assets/noise.svg)] px-32 py-5 sticky top-0">
+      <div className="max-w-7xl flex justify-between items-center text-[#00391B] font-medium">
+        {/* Logo */}
         <div>
-          <button className="bg-[#00ED85] px-4 py-3 rounded-md">
-            JOIN THE WAITLIST
-          </button>
+          <img src="/assets/logo-full.svg" alt="Quizard logo" />
         </div>
+
+        {/* Nav */}
+        <nav className="flex space-x-5 items-center">
+          <div>Features</div>
+          <div>About us</div>
+          <div>FAQ</div>
+          <div>
+            <button className="bg-[#00ED85] px-4 py-3 rounded-md">
+              JOIN THE WAITLIST
+            </button>
+          </div>
+        </nav>
       </div>
-    </div>
+    </header>
   );
 }
