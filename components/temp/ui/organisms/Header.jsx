11e7872bcfb9f78f9d717efcb5ed6fe5c67580f2
Divine Orji <dpkreativ@gmail.com>
2023-03-07 16:50:44 +0100

    refactor: updates colors to match UI

--- components/temp/ui/organisms/Header.jsx
+++ components/temp/ui/organisms/Header.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="bg-[#C4FBDD] bg-[url(/assets/noise.svg)] px-32 py-5 sticky top-0">
-      <div className="max-w-7xl mx-auto flex justify-between items-center text-[#00391B] font-medium">
+      <div className="max-w-7xl mx-auto flex justify-between items-center font-medium">
         {/* Logo */}
         <div>
           <img src="/assets/logo-full.svg" alt="Quizard logo" />
         </div>
 
         {/* Nav */}
-        <nav className="flex space-x-5 items-center">
+        <nav className="flex space-x-5 items-center text-[#00210E]">
           <div>Features</div>
           <div>About us</div>
           <div>FAQ</div>
           <div>
-            <button className="bg-[#00ED85] px-4 py-3 rounded-md">
+            <button className="bg-[#00ED85] text-[#00391B] px-4 py-3 rounded-md">
               JOIN THE WAITLIST
             </button>
           </div>
--- pages/coming-soon.js
+++ pages/coming-soon.js
@@ -8,10 +8,10 @@ export default function ComingSoon() {
         <div className="max-w-7xl mx-auto grid grid-cols-2 items-center">
           {/* Left side */}
           <div className="grid gap-5">
-            <h1 className="font-semibold text-5xl leading-snug">
+            <h1 className="font-semibold text-5xl leading-snug text-[#00391B]">
               Engage & Interact with your Audience
             </h1>
-            <p className="text-2xl font-light">
+            <p className="text-2xl font-light text-[#00210E]">
               Quizard is an all-in-one platform for engaging your audience with
               interactive quiz sessions and collect valuable results/feedback.
             </p>
@@ -19,7 +19,7 @@ export default function ComingSoon() {
             {/* Waitlist form */}
             <div>
               <form>
-                <label htmlFor="" className="text-sm">
+                <label htmlFor="" className="text-sm text-[#00391B]">
                   Join the waitlist to get notified when we launch
                 </label>
                 <div className="w-full max-w-md flex border border-[#00A65B] rounded-md p-1">
@@ -28,11 +28,11 @@ export default function ComingSoon() {
                     name="waitlist_email"
                     id="waitlist_email"
                     placeholder="Email address"
-                    className="w-full outline-none bg-transparent rounded-md m-3"
+                    className="w-full outline-none bg-transparent rounded-md m-3 placeholder:text-[#006D3A]"
                   />
                   <button
                     type="submit"
-                    className="bg-[#00ED85] text-green-200 px-8 py-3 rounded-md"
+                    className="bg-[#00ED85] text-[#006D3A] px-8 py-3 rounded-md"
                   >
                     Join
                   </button>
