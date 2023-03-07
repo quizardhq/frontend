31ae194494714ea0093bbb1769ce4b045d7f33f5
Divine Orji <dpkreativ@gmail.com>
2023-03-07 15:14:19 +0100

    feat: adds Quizard logo, builds Header component
    In this commit I did the following:
    - Built out the Header component for Desktop screens
    - Added the full logo as an SVG file

--- components/temp/ui/organisms/Header.jsx
+++ components/temp/ui/organisms/Header.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
 export default function Header() {
-  return <div className="bg-green-300 bg-[url(/assets/noise.svg)]">Head</div>;
+  return (
+    <div className="bg-green-300 bg-[url(/assets/noise.svg)] flex justify-between px-32 py-5 items-center text-[#00391B] font-medium">
+      {/* Logo */}
+      <div>
+        <img src="/assets/logo-full.svg" alt="Quizard logo" />
+      </div>
+
+      {/* Nav */}
+      <div className="flex space-x-5 items-center">
+        <div>Features</div>
+        <div>About us</div>
+        <div>FAQ</div>
+        <div>
+          <button className="bg-[#00ED85] px-4 py-3 rounded-md">
+            JOIN THE WAITLIST
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
