4d5ddc0880aab11c0419096dcf732f950fdc9d5d
CeoFred <johnsonmessilo19@gmail.com>
2023-03-12 18:53:56 +0100

    chore: Favicon - removed unused files - added single logo without name

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }) {
           content="Created by Quizard Open Source team"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/assets/quizzard.ico" />
+        <link rel="icon" href="/assets/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </div>
