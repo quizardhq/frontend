cc49c557da2b53abaa5a0ccd9ccf5a1664cfd759
Divine Orji <dpkreativ@gmail.com>
2023-03-15 13:42:49 +0100

    refactor: remove redundant bg-color code

--- layouts/HomeLayout.jsx
+++ layouts/HomeLayout.jsx
@@ -2,7 +2,7 @@ import Header from '@/components/ui/organisms/Header';
 
 export default function HomeLayout({ children }) {
   return (
-    <div className="bg-[#F5FFF3]">
+    <div>
       <Header />
       <main>{children}</main>
     </div>
