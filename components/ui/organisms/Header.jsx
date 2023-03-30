d9ecf62f3108d69e834d12daa8f58dcc024aae7c
whalewalker <mongodbms@gmail.com>
2023-03-30 11:00:09 +0100

    Fix ESLint warning: Do not use an <a> element to navigate to /. Use <Link /> from next/link instead.

--- components/ui/organisms/Header.jsx
+++ components/ui/organisms/Header.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from "next/link";
 import { MenuIcon, QuizardFullLogo } from '@/components/assets';
 import Button from '@/components/ui/atoms/Button';
 
@@ -7,13 +8,13 @@ export default function Header() {
     <header className="bg-[#C4FBDD] bg-[url(/assets/noise.svg)] p-5 lg:px-32 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center font-medium">
         {/* Logo */}
-        <a href="#" className="h-6 lg:h-max cursor-pointer">
+        <Link href="/" className="h-6 lg:h-max cursor-pointer">
           <Image
             src={QuizardFullLogo}
             alt="Quizard logo"
             className="h-full w-max"
           />
-        </a>
+        </Link>
 
         {/* Nav */}
         <div className="lg:hidden">
@@ -21,13 +22,16 @@ export default function Header() {
         </div>
 
         <nav className="hidden lg:flex font-medium space-x-5 items-center text-[#00210E] cursor-pointer">
-          <a href="#features">Features</a>
-          <a href="#about-us">About Us</a>
-          <a href="#faq">FAQ</a>
+          <Link href="/#features">Features</Link>
+          <Link href="/#about-us">About Us</Link>
+          <Link href="/#faq">FAQ</Link>
           <div>
-            <Button submit className="primary-btn text-[#00391B] font-medium">
-              JOIN THE WAITLIST
-            </Button>
+            <Link href="/">
+              <Button submit className="primary-btn text-[#00391B] font-medium">
+                JOIN THE WAITLIST
+              </Button>
+            </Link>
+
           </div>
         </nav>
       </div>
