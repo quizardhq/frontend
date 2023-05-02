b2b7092661721353e0e4194c10bb34759f767d77
CeoFred <johnsonmessilo19@gmail.com>
2023-05-02 10:42:24 +0100

    feat: Typescript Migration
    - added typescript config
    - renamed jsx files
    - fixed font size for coming soon page
    - added extra content for coming soon page

---
+++ components/ui/atoms/Button.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from 'react';
+
+interface ButtonProps{
+  children?: ReactNode;
+  submit?: boolean;
+  className?: string;
+  // other props go here
+}
+
+
+const Button: React.FC<ButtonProps> = function({ children, submit, className }) {
+  return (
+    <div>
+      <button type={submit ? 'submit' : 'button'} className={className}>
+        {children}
+      </button>
+    </div>
+  );
+}
+
+Button.defaultProps = {
+  children: null, // or any other default value you want to provide
+  submit: false,
+  className: ''
+};
+
+export default Button
