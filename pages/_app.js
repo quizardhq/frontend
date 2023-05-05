fc80382195240696ded78546a63dcf95df6eb539
CeoFred <johnsonmessilo19@gmail.com>
2023-05-05 19:34:15 +0100

    ref: Home SEO
    - Added metatag to home

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '@/styles/globals.css';
-import Head from 'next/head';
 import localFont from 'next/font/local';
 
 const mena = localFont({
@@ -29,46 +28,6 @@ const mena = localFont({
 export default function App({ Component, pageProps }) {
   return (
     <div className={`${mena.variable} font-sans bg-[#F5FFF3]`}>
-      <Head>
-        <title>Quizard - Engage and Interact with your Audience</title>
-        <meta
-          name="title"
-          content="Quizard - Engage and Interact with your Audience"
-        />
-        <meta
-          name="description"
-          content="Quizard is an all-in-one platform for engaging your audience with interactive quiz sessions"
-        />
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://quizardhq.com/" />
-        <meta
-          property="og:title"
-          content="Quizard - Engage and Interact with your Audience"
-        />
-        <meta
-          property="og:description"
-          content="Quizard is an all-in-one platform for engaging your audience with interactive quiz sessions"
-        />
-        <meta property="og:image" content="/assets/meta-image.png" />
-
-        {/* Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://quizardhq.com/" />
-        <meta
-          property="twitter:title"
-          content="Quizard - Engage and Interact with your Audience"
-        />
-        <meta
-          property="twitter:description"
-          content="Quizard is an all-in-one platform for engaging your audience with interactive quiz sessions"
-        />
-        <meta property="twitter:image" content="meta-image.png" />
-
-        {/* Others */}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/assets/favicon.ico" />
-      </Head>
       <Component {...pageProps} />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import ComingSoon from '@/components/ui/organisms/ComingSoon';
 import HomeLayout from '@/layouts/HomeLayout';
+import MetaTag from '@/components/ui/atoms/MetaTag';
 
 export default function Home() {
   return (
     <HomeLayout>
+      <MetaTag
+        title="Quizard - Engage and Interact with your Audience"
+        description="Quizard is an all-in-one platform for engaging your audience with interactive quiz sessions "
+        url="/"
+      />
       <ComingSoon />
     </HomeLayout>
   );
