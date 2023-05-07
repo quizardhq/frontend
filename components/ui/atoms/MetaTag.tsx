30f04f6baf2978f4553aa6e1c60a03882fc7a9a5
CeoFred <johnsonmessilo19@gmail.com>
2023-05-07 19:56:05 +0100

    ref: og image
    - use remore url

--- components/ui/atoms/MetaTag.tsx
+++ components/ui/atoms/MetaTag.tsx
@@ -10,14 +10,20 @@ export default function MetaTag({ title, description, url }) {
 
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content="/assets/og:image.png" />
+      <meta
+        property="og:image"
+        content="https://quizardhq.com/assets/og:image.png"
+      />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content="/assets/og:image.png" />
+      <meta
+        property="twitter:image"
+        content="https://quizardhq.com/assets/og:image.png"
+      />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@quizardHQ" />
       <meta property="og:url" content="https://quizardhq.com" />
