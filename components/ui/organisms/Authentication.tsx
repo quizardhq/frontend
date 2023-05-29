59c61d633d767a433222661e99c56cf0cde10b8c
Divine Orji <dpkreativ@gmail.com>
2023-05-29 18:29:01 +0100

    feat: sign up ui done

--- components/ui/organisms/Authentication.tsx
+++ components/ui/organisms/Authentication.tsx
@@ -2,7 +2,7 @@ import Button from '../atoms/Button';
 
 export function SignUp() {
   return (
-    <div className="p-5 my-20 grid gap-8">
+    <div className="p-5 my-20 grid gap-8 max-w-xl mx-auto">
       {/* Heading */}
       <section className="text-center mx-auto grid gap-2">
         <h1 className="font-bold text-2xl lg:text-3xl">Create an Account</h1>
@@ -41,55 +41,96 @@ export function SignUp() {
 
       {/* Form Auth */}
       <section>
-        <form>
+        <form className="grid gap-5">
           <div>
             <label htmlFor="signUp_firstName">
-              <div>First Name</div>
-              <div>
-                <input type="text" name="firstName" id="signUp_firstName" />
+              <div className="text-sm mb-1">First Name</div>
+              <div className="border border-black rounded-md px-1 py-2">
+                <input
+                  type="text"
+                  name="firstName"
+                  id="signUp_firstName"
+                  className="w-full bg-transparent outline-none"
+                  placeholder="John"
+                />
+              </div>
+            </label>
+          </div>
+
+          <div>
+            <label htmlFor="signUp_lastName">
+              <div className="text-sm mb-1">Last Name</div>
+              <div className="border border-black rounded-md px-1 py-2">
+                <input
+                  type="text"
+                  name="lastName"
+                  id="signUp_lastName"
+                  className="w-full bg-transparent outline-none"
+                  placeholder="Doe"
+                />
               </div>
             </label>
           </div>
 
           <div>
             <label htmlFor="signUp_email">
-              <div>Email</div>
-              <div>
-                <input type="email" name="email" id="signUp_email" />
+              <div className="text-sm mb-1">Email</div>
+              <div className="border border-black rounded-md px-1 py-2">
+                <input
+                  type="email"
+                  name="email"
+                  id="signUp_email"
+                  className="w-full bg-transparent outline-none"
+                  placeholder="example@gmail.com"
+                />
               </div>
             </label>
           </div>
 
           <div>
             <label htmlFor="signUp_password">
-              <div>Password</div>
-              <div>
-                <input type="password" name="password" id="signUp_password" />
+              <div className="text-sm mb-1">Password</div>
+              <div className="border border-black rounded-md px-1 py-2">
+                <input
+                  type="password"
+                  name="password"
+                  id="signUp_password"
+                  className="w-full bg-transparent outline-none"
+                  placeholder="******"
+                />
               </div>
             </label>
           </div>
 
           <div>
-            <label htmlFor="signUp_receiveQuizardInfo">
+            <label
+              htmlFor="signUp_receiveQuizardInfo"
+              className="flex items-center gap-3"
+            >
               <input
                 type="checkbox"
                 name="receiveQuizardInfo"
                 id="signUp_receiveQuizardInfo"
               />
-              <div>
+              <div className="text-sm">
                 I wish to receive information, offers, and updates from Quizard
               </div>
             </label>
           </div>
 
           <div>
-            <button type="submit">Sign up</button>
+            <Button
+              isSubmit
+              className="w-full bg-[#00A65B] rounded-md text-white p-3"
+            >
+              Sign up
+            </Button>
           </div>
         </form>
       </section>
 
       {/* Choose to login instead */}
-      <section>
+      <section className="flex gap-3 justify-center items-center text-sm">
         <div>Already have an account?</div>
         <div>log in</div>
       </section>
